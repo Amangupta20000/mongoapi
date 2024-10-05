690c9b74aff7e5fa45d1e3e9010a4d752f516d4c
Aman Gupta <amangupta20000@gmail.com>
2024-10-06 00:12:27 +0530

    Go : port changed

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Amangupta20000/mongoapi/router"
 )
 
 func main() {
+	// Get the port from the environment variable, default to 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Fallback to 8080 if not set
+	}
+
 	fmt.Println("MongoDb")
 	r := router.Router()
-	fmt.Println("Server is getting Started...")
-	log.Fatal(http.ListenAndServe(":5000", r))
-	fmt.Println("Listening on PORT 5000 ...")
+	fmt.Printf("Server is getting Started on PORT %s...\n", port)
 
+	// Listen and serve on the specified port
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
